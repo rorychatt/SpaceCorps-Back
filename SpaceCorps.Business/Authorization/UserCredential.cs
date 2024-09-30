ce54de1d19c28c8a70101c760ab31a38a5097c1d
rorychatt <pavel.rinne@gmail.com>
2024-09-30 10:44:36 +0200

    feat: user credential with hashing

--- SpaceCorps.Business/Authorization/UserCredential.cs
+++ SpaceCorps.Business/Authorization/UserCredential.cs
@@ -1,4 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SpaceCorps.Business.Authorization;
 
@@ -8,9 +11,39 @@ public class UserCredential
     public int Id { get; set; }
     [Required]
     public string Email { get; init; } = null!;
-    [Required]
+    [NotMapped]
     public string Password { get; init; } = null!;
-    public string? Salt { get; init; }
-    public string? Hash { get; init; }
-    
+    public string Salt { get; init; }
+    public string Hash { get; init; }
+
+    public UserCredential(string email, string password)
+    {
+        Email = email;
+        Password = password;
+        Salt = GenerateSalt();
+        Hash = HashPassword();
+    }
+
+    private string GenerateSalt()
+    {
+        byte[] saltBytes = new byte[16];
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            rng.GetBytes(saltBytes);
+        }
+        return Convert.ToBase64String(saltBytes);
+    }
+
+    private string HashPassword()
+    {
+        var hashedBytes = SHA512.HashData(Encoding.UTF8.GetBytes(Password + Salt));
+        return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+    }
+
+    public bool VerifyPassword(string password)
+    {
+        var hashedBytes = SHA512.HashData(Encoding.UTF8.GetBytes(password + Salt));
+        var hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+        return hash == Hash;
+    }
 }
